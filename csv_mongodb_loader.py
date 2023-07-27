8e204aeb8e2d87566a57afafdf720dca2ea056f2
lilJuan4168 <chesteraraoz12.0@gmail.com>
2023-07-27 14:54:50 -0300

    update

--- csv_mongodb_loader.py
+++ csv_mongodb_loader.py
@@ -1,7 +1,7 @@
 import pymongo
 import pandas as pd
 import json
-
+from time import sleep
 
 with open('credentials.json', 'r') as cred:
     credentials = json.load(cred)
@@ -10,7 +10,8 @@ username = credentials['username']
 password = credentials['password']
 
 #connection with mongodb
-client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster-palta-cpa.nyymbgr.mongodb.net/")
+#client = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster-palta-cpa.nyymbgr.mongodb.net/")
+client = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
 
 db = client['codigoPostalArgentino']
 collection = db['cpa']
@@ -21,12 +22,19 @@ data = pd.read_csv("codigosCPA.csv")
 data = data.to_dict(orient='records')
 
 #data load into mongodb by parts due to ram consumption
+
+print('\n>>Loading by parts:')
 collection.insert_many(data[0:2829239])
-
+print('1/4 loaded')
+sleep(5)
 collection.insert_many(data[2829239:5658478])
-
+print('2/4 loaded')
+sleep(5)
 collection.insert_many(data[5658478:8487717])
-
+print('3/4 loaded')
+sleep(5)
 collection.insert_many(data[8487717:11316956])
-
+print('4/4 loaded')
+sleep(5)
+print('completed')
 
